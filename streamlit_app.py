88b7f34455f3505ea0d85ece23766e5a0be86e3e
Misco31 <gabrimisco@gmail.com>
2024-09-12 17:38:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,18 @@ def get_last_post_date(username):
         last_post = next(posts)
         return f"L'ultimo post di {username} è stato pubblicato il {last_post.date}"
     except Exception as e:
-        return f"Errore: {e}"
+        return f"Errore nel recupero delle informazioni per {username}: {e}"
 
 # Creazione dell'interfaccia con Streamlit
 st.title('Controlla gli ultimi post su Instagram')
+
+# Input per inserire il nome utente
 username = st.text_input('Inserisci un username di Instagram:')
 
 # Bottone per eseguire il controllo
 if st.button('Controlla'):
-    result = get_last_post_date(username)
-    st.write(result)
+    if username:
+        result = get_last_post_date(username)
+        st.write(result)
+    else:
+        st.write("Per favore, inserisci un nome utente valido.")
